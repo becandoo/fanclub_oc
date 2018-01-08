02ab171205abc61d27d978602f3ae9c8cccf8815
Brandon Cranmore <brandoncranmore@gmail.com>
2018-01-08 08:10:15 -0800

    fixed js imports

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,12 +1,9 @@
 import $ from 'jquery';
-import whatInput from 'what-input';
+import Popper from 'popper.js';
 
 window.$ = $;
 
-import Foundation from 'foundation-sites';
+import Bootstrap from 'bootstrap';
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
 //import './lib/foundation-explicit-pieces';
-
-
-$(document).foundation();
